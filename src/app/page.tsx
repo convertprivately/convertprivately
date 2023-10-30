a7ec5deae66db1ffcf5af1c74c9b75df884ffd61
Egemen Göl <egemengol@gmail.com>
2023-10-31 02:09:06 +0300

    polish landing a bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 export default function Landing() {
   return (
     <div className="container" style={{width: "70%"}}>
+
+      <p>Maybe a description here</p>
+
+      <div style={{display: "flex", flexDirection: "column", justifyContent: "center", height: "60vh"}}>
       <div style={{display: "flex", flexDirection: "row", width: "100%", justifyContent: "space-between"}}>
         <div>
           Want the audio from a video?
@@ -13,7 +17,7 @@ export default function Landing() {
           <a href="/spaces">Download it here</a>
         </div>
       </div>
-      
+      </div>
     </div>
   )
 }
