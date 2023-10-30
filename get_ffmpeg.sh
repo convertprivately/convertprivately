f6f6720464de09248cc9e984109ee8bd3dafa44b
Egemen Göl <egemengol@gmail.com>
2023-10-31 01:09:47 +0300

    multithread links

--- get_ffmpeg.sh
+++ get_ffmpeg.sh
@@ -1,5 +1,9 @@
 # Download file from given url to public folder
 
+mkdir -p ./public
 curl -o ./public/ffmpeg-core.js https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd/ffmpeg-core.js
 curl -o ./public/ffmpeg-core.wasm https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd/ffmpeg-core.wasm
+# curl -o ./public/ffmpeg-core.js https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/umd/ffmpeg-core.js
+# curl -o ./public/ffmpeg-core.wasm https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/umd/ffmpeg-core.wasm
+# curl -o ./public/ffmpeg-core.worker.js https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/umd/ffmpeg-core.worker.js
 
