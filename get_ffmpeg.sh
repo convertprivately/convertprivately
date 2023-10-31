bb1fe8d773e91cd283acf7f7dd31b8145fc8ab9f
Egemen Göl <egemengol@gmail.com>
2023-10-31 14:47:02 +0300

    Fix URL

--- get_ffmpeg.sh
+++ get_ffmpeg.sh
@@ -2,10 +2,10 @@
 
 mkdir -p ./public
 if [ ! -f ./public/ffmpeg-core.js ]; then
-    curl -o ./public/ffmpeg-core.js https://raw.githubusercontent.com/ffmpegwasm/ffmpeg.wasm/0.12.4/ffmpeg-core.js
+    curl -o ./public/ffmpeg-core.js https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd/ffmpeg-core.js
 fi
 if [ ! -f ./public/ffmpeg-core.wasm ]; then
-    curl -o ./public/ffmpeg-core.wasm https://raw.githubusercontent.com/ffmpegwasm/ffmpeg.wasm/0.12.4/ffmpeg-core.wasm
+    curl -o ./public/ffmpeg-core.wasm https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd/ffmpeg-core.wasm
 fi
 # curl -o ./public/ffmpeg-core.js https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/umd/ffmpeg-core.js
 # curl -o ./public/ffmpeg-core.wasm https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/umd/ffmpeg-core.wasm
