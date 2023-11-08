2bdce98ce3c2e3524db5758d96acfd1545b6ad84
chinchin <86435165+chinekop@users.noreply.github.com>
2023-11-08 13:28:58 +0300

    fix + explainer

--- src/Explainer.tsx
+++ src/Explainer.tsx
@@ -6,6 +6,15 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 export function Explainer() {
   return (
     <div className="flex flex-col  mx-auto max-w-[900px] pb-20 sm:px-6 lg:px-0 mt-20">
+      <div className="flex flex-col  p-4">
+      <p className="mt-4 text-lg font-bold text-zinc-300 ">
+      What is ConvertPrivately and how does it work?
+        </p>
+        <p className="mt-4 text-sm text-zinc-300 ">
+      Conversion is a fairly simple process. You can do it directly within your browser, with no need to upload files to a server or install additional software. <br/><br/><span className="font-semibold">ConvertPrivately</span> is a completely free audio and video conversion tool that uses FFMPEG to convert files using browser resources, assuring convenience and privacy.
+      </p>
+
+      </div>
       <div className="flex flex-col sm:flex-row justify-between max-w-screen-lg mx-auto sm:my-8">
         {/* Single Feature */}
         <div className="flex flex-col p-4  sm:w-1/2">
@@ -15,7 +24,7 @@ export function Explainer() {
             <h2 className="font-bold text-lg ml-4 text-white">Private</h2>
           </div>
           <p className="mt-4 text-sm text-zinc-300 ">
-            ConvertPrivately uses FFMPEG to handle all the process in your browser. We do not
+            What you upload to ConvertPrivately is between you and your browser. We do not
             see or store your files. Your data stays yours.{" "}
           </p>
         </div>
@@ -43,9 +52,8 @@ export function Explainer() {
             <h2 className="font-bold text-lg ml-4 text-white">Free</h2>
           </div>
           <p className="mt-4 text-sm text-zinc-300">
-          ConvertPrivately is a free tool. We don’t charge for conversions and we
-            don’t have any ads, and we intend to do neither in the foreseeable
-            future.{" "}
+          ConvertPrivately is a free tool. We do not charge for conversions and we
+            don’t have any ads.{" "}
           </p>
         </div>
 
